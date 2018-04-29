3456364cb0dff2d2aca5accc919e1020687b12a0
Rudra Madhav Biswal <35427300+phantom-5@users.noreply.github.com>
2018-04-29 18:58:40 +0530

    minor change to description

--- distance_sensor/distance_sensor.ino
+++ distance_sensor/distance_sensor.ino
@@ -1,1 +1,31 @@
-/*author - phantom-5project - Blink*/int led = 13;int trigPin =10;int echoPin=12;void setup() {    pinMode(led,OUTPUT);    pinMode(trigPin,OUTPUT);    pinMode(echoPin,INPUT);    }void loop() {    float duration,distance;    digitalWrite(trigPin,LOW);    delayMicroseconds(2);    digitalWrite(trigPin,HIGH);    delayMicroseconds(10);    digitalWrite(trigPin,LOW);    duration=pulseIn(echoPin,HIGH);    distance=(duration/2)*0.03444;    if(distance>=40){      digitalWrite(led,HIGH);    }else{      digitalWrite(led,LOW);    }}
+/*
+author - phantom-5
+project - distance_sensing
+*/
+
+int led = 13;
+int trigPin =10;
+int echoPin=12;
+
+void setup() {
+    pinMode(led,OUTPUT);
+    pinMode(trigPin,OUTPUT);
+    pinMode(echoPin,INPUT);
+    
+}
+
+void loop() {
+    float duration,distance;
+    digitalWrite(trigPin,LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin,HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin,LOW);
+    duration=pulseIn(echoPin,HIGH);
+    distance=(duration/2)*0.03444;
+    if(distance>=40){
+      digitalWrite(led,HIGH);
+    }else{
+      digitalWrite(led,LOW);
+    }
+}
